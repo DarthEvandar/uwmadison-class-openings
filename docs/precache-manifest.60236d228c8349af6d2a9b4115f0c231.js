d359878a77d16ad8d69c0ce782fbab2245d6d60b
Anders Sundheim <andersundheim@gmail.com>
2019-01-31 23:59:40 -0600

    fixed base url

---
+++ docs/precache-manifest.60236d228c8349af6d2a9b4115f0c231.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0a4a90b94e4f7cb8781",
+    "url": "/uwmadison-class-openings/static/js/runtime~main.e0a4a90b.js"
+  },
+  {
+    "revision": "0a1bd294112ef2507f29",
+    "url": "/uwmadison-class-openings/static/js/main.0a1bd294.chunk.js"
+  },
+  {
+    "revision": "f9bb55083e64d1837bb0",
+    "url": "/uwmadison-class-openings/static/js/1.f9bb5508.chunk.js"
+  },
+  {
+    "revision": "0a1bd294112ef2507f29",
+    "url": "/uwmadison-class-openings/static/css/main.90af9964.chunk.css"
+  },
+  {
+    "revision": "c9718c4aa239de4c1b78e22166638cc1",
+    "url": "/uwmadison-class-openings/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e4c7fa9c00ff92034e388b810d3bb8ec.js"
+  "/uwmadison-class-openings/precache-manifest.60236d228c8349af6d2a9b4115f0c231.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/uwmadison-class-openings/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
